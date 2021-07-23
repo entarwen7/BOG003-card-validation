e0bcd4edd58a85e09a060866e92e432c5a29f227
Alejandra Arevalo <entarwen07@hotmail.com>
2021-07-23 16:42:36 -0500

    Proyecto con isValid y maskify terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+let inputAdd = document.getElementById("inputNumero");
+//console.log(inputAdd, "numero recibido de tarjeta" );
+
+
+document.getElementById("btn").addEventListener("click", () => {
+  let valorInput = validator.isValid(inputAdd.value);
+  //console.log(valorInput);// arroja verdadero o falso.
+
+  let maskify3 = validator.maskify(inputAdd.value);
+  //console.log(maskify3);// muestra la mascara
+  
+   document.getElementById("inputRespuesta").value = maskify3;
+
+   if(valorInput === true){
+     document.getElementById("mensajeValido").innerHTML="Tarjeta valida"
+   }else{
+    document.getElementById("mensajeValido").innerHTML="Tarjeta NO valida"
+   }
+
+}); // cierra el 1° document
+/* funcion del imput del formulario */
+
+document.getElementById("validarInformacion").addEventListener("click", () => { vaResultado() });
+
+/*Dar la funcionalidad ala interfaz */
+function vaResultado() {
+  document.getElementById("primera").style.display = "none"
+  document.getElementById("segunda").style.display = "block"
+}
+
+document.getElementById("respuesta").addEventListener("click", () => { vaResultado2() });
+
+/*Dar la funcionalidad ala interfaz, prueba de retorno a la pagina principal */
+function vaResultado2() {
+  document.getElementById("segunda").style.display = "none"
+  document.getElementById("primera").style.display = "block"
+
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
 const validator = {
-  // ...
-};
+  isValid: function (inputNumero) {
 
+    var numberVa = inputNumero.split("");
+    let numberLunh = numberVa.reverse();
+    let arraySuma = 0;
+    let sumaImpar = 0;
+    let sumaTotal = 0;
+    let sumaPar = 0;
+
+    for (let i = 0; i < numberLunh.length; i++) {// si el número es impar se multiplica por 2, buscar la posicion y multiplicar,
+      if (i % 2 === 1) {// posición Par 1}
+        sumaPar = numberLunh[i] * 2; //multiplicación posición.
+        if (sumaPar >= 10) {
+          arraySuma = sumaPar.toString().split("")// se vuelve string y se separa.
+          numberLunh[i] = parseInt(arraySuma[0]) + parseInt(arraySuma[1])// se suman entres si para dar un solo número.          
+        } else {
+          numberLunh[i] = sumaPar;// actualiza el valor de los Números par , que no se deben multiplicar
+        }
+        sumaTotal = parseInt(sumaTotal) + parseInt(numberLunh[i]);
+      } else if (i % 2 === 0) {
+        sumaImpar = parseInt(sumaImpar) + parseInt(numberLunh[i])
+      }//almacenar los numeros impares.
+    }// aqui cierra for 
+
+    sumaTotal = sumaTotal + sumaImpar;// se suma los pares e impares.
+
+    if (sumaTotal % 10 == 0) {
+  
+      return true
+    }
+    else {
+   
+      return false;
+    }
+
+  }// aquí se cierra la función
+  ,// aquí inicia el mastify
+
+  maskify: function (inputNumero) {
+
+    let numberHidden = inputNumero.length;
+    let masked = "";
+      for (let i = 0; i < numberHidden - 4; i++) {
+      masked += "#";
+    }
+    let maskeDigit = masked + inputNumero.substring(numberHidden - 4, numberHidden);
+    return  maskeDigit
+    //console.log(maskeDigit);
+      }
+
+  }
+  ;
 export default validator;
